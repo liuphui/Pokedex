26c15e6b669a80121f25186a6772bd1796e410ac
Peng Hui Liu <liup1@tcd.ie>
2026-02-08 15:17:30 +0000

    Changed tests and removed get_type

--- backend/app/services/pokeapi_client.py
+++ backend/app/services/pokeapi_client.py
@@ -5,7 +5,3 @@ BASE_URL = "https://pokeapi.co/api/v2/"
 def get_pokemon(name_or_id):
     url = f"{BASE_URL}/pokemon/{name_or_id}"
     return cached_get(url)
-
-def get_type(type):
-    url = f"{BASE_URL}/type/{type}"
-    return cached_get(url)
--- backend/tests/test_cache.py
+++ backend/tests/test_cache.py
@@ -1,9 +1,9 @@
 from app.services.pokeapi_client import get_pokemon
 
-print("First call")
+print("First call - get_pokemon")
 data = get_pokemon("pikachu")
-print(data["name"], data["id"])
+print(data["name"], data["id"], data["types"][0]["type"]["name"], "\n")
 
-print("Second call")
+print("Second call - get_pokemon")
 data = get_pokemon("pikachu")
-print(data["name"], data["id"])
+print(data["name"], data["id"], data["types"][0]["type"]["name"], "\n")
