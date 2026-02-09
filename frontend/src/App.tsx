ba6b6ed31749f42a12760187b60901b47e99e81b
Peng Hui Liu <liup1@tcd.ie>
2026-02-09 21:44:00 +0000

    Implement Pokemon fetching and display in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react' 
+import { getPokemon, type Pokemon } from "./api/pokemon";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemon, setPokemon] = useState<Pokemon | null>(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    async function loadPokemon() {
+      const data = await getPokemon("pikachu")
+      setPokemon(data)
+    }
+
+    loadPokemon();
+  }, []);
+
+  return(
+    <div>
+      <h1>Pokedex</h1>
+      {pokemon ? (
+        <div>
+          <h2>{pokemon.name}</h2>
+          <img src={pokemon.sprite ?? ""} alt={pokemon.name}></img>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
 
 export default App
---
+++ frontend/src/api/pokemon.ts
@@ -0,0 +1,15 @@
+export interface Pokemon {
+    id: number,
+    name: string
+    types: string[]
+    sprite: string | null
+}
+
+export async function getPokemon(name: string): Promise<Pokemon>{
+    const response = await fetch(`/api/pokemon/${name}`);
+    if (!response.ok){
+        throw new Error("Pokemon not found")
+    }
+    const data = await response.json();
+    return data
+}
