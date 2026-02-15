c356adc4c6ce8a7e6de1bf3ed118ec91480a1464
Peng Hui Liu <liup1@tcd.ie>
2026-02-15 22:23:26 +0000

    Removed App.css and added an image header for the website

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,59 +9,67 @@ function App() {
   async function handleSearch() {
     const data = await getPokemon(query)
     setPokemon(data)
-    console.log(data)
   }
 
   return (
-    <div>
-
-      <div className="">
-        <h1>Pokédex</h1>
-      </div>
-
-      <div className="">
-        <input
-          type="text"
-          placeholder="Enter Pokémon name..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        <button onClick={handleSearch}>Submit</button>
-      </div>
-
-      <div className="relative w-full max-w-md">
-        {/* Base pokedex image */}
+    <div className="w-screen overflow-x-hidden">
+      <header className="w-screen block m-0 p-0">
         <img
-          src="/pokedex.png"
-          alt="pokedex-img"
+          src="/pokedex-header.png"
+          alt="pokedex-header"
           className="w-full h-auto block"
         />
+      </header>
 
-        {/* Screen area (fixed zone) */}
-        <div className="absolute top-[13%] left-[5%] w-[45%] h-[32%] flex items-center justify-center">
+      <div className="text-center">
+        <div className="max-w-5xl mx-auto p-8">
+        <h1>Pokédex</h1>
+
+        <div className="">
+          <input
+            type="text"
+            placeholder="Enter Pokémon name..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button onClick={handleSearch}>Submit</button>
+        </div>
+
+        <div className="relative w-full max-w-md">
+          {/* Base pokedex image */}
+          <img
+            src="/pokedex.png"
+            alt="pokedex-img"
+            className="w-full h-auto block"
+          />
+
+          {/* Screen area (fixed zone) */}
+          <div className="absolute top-[13%] left-[5%] w-[45%] h-[32%] flex items-center justify-center">
+            {pokemon && (
+              <img
+                src={pokemon.sprite ?? ""}
+                alt={pokemon.name}
+                className="max-w-full max-h-full object-contain"
+              />
+            )}
+          </div>
+
           {pokemon && (
-            <img
-              src={pokemon.sprite ?? ""}
-              alt={pokemon.name}
-              className="max-w-full max-h-full object-contain"
-            />
+            <div className="font-medium">
+              <div>HP: {pokemon.stats["hp"]}</div>
+              <div>Attack: {pokemon.stats["attack"]}</div>
+              <div>Defense: {pokemon.stats["defense"]}</div>
+              <div>SpAtk: {pokemon.stats["special-attack"]}</div>
+              <div>SpDef: {pokemon.stats["special-defense"]}</div>
+              <div>Speed: {pokemon.stats["speed"]}</div>
+              <div>Total Base Stats: {calculateTotalStats(pokemon)}</div>
+            </div>
           )}
         </div>
 
-        {pokemon && (
-          <div className="font-medium">
-            <div>HP: {pokemon.stats["hp"]}</div>
-            <div>Attack: {pokemon.stats["attack"]}</div>
-            <div>Defense: {pokemon.stats["defense"]}</div>
-            <div>SpAtk: {pokemon.stats["special-attack"]}</div>
-            <div>SpDef: {pokemon.stats["special-defense"]}</div>
-            <div>Speed: {pokemon.stats["speed"]}</div>
-            <div>Total Base Stats: {calculateTotalStats(pokemon)}</div>
-          </div>
-        )}
+        {!pokemon && <p>No Pokémon name entered</p>}
+        </div>
       </div>
-
-      {!pokemon && <p>No Pokémon name entered</p>}
     </div>
   );
 }
