92fb5f75fbcf65ca0dc48527ae82ec2b0f9809bc
Peng Hui Liu <liup1@tcd.ie>
2026-02-16 19:47:27 +0000

    Fixed search space

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,6 +15,7 @@ function App() {
   return (
     <div className="w-screen">
       <PokeBallBackground></PokeBallBackground>
+
       <div className="w-full overflow-x-hidden">
         <header className="m-0 bg-red-400 text-white text-xl font-bold px-6 py-6">
           <div className="flex justify-between w-auto h-16">
@@ -31,16 +32,16 @@ function App() {
       <div className="text-center">
         <div className="max-w-5xl mx-auto p-8">
 
-          <div className="">
+          <div className="flex justify-start bg-amber-50 gap-8">
             <input
               type="text"
               placeholder="Enter Pokémon name..."
               value={query}
               onChange={(e) => setQuery(e.target.value)}
+              className="px-4 py-2 flex-1"
             />
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xs" onClick={handleSearch}>Submit</button>
           </div>
-          {!pokemon && <p>No Pokémon name entered</p>}
         </div>
 
         <div className="relative w-full max-w-md mx-auto">
