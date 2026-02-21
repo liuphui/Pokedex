614a549976155e743c64cbc31a6b815c8efa0bc1
Peng Hui Liu <liup1@tcd.ie>
2026-02-21 14:50:56 +0000

    Changed text for base stats information and displays a stats bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { getPokemon, type Pokemon } from "./api/pokemon";
-import { calculateTotalStats } from './utils/stats';
+import { calculateTotalStats, formatStatName } from './utils/stats';
 import PokeBallBackground from "./components/PokeBallBackground";
 
 function App() {
@@ -29,51 +29,65 @@ function App() {
         </header>
       </div>
 
-      <div className="text-center">
-        <div className="max-w-5xl mx-auto p-8">
+      <div className="max-w-5xl mx-auto p-8">
+        <div className="flex justify-start bg-amber-50 gap-8">
+          <input
+            type="text"
+            placeholder="Enter Pokémon name..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            className="px-4 py-2 flex-1"
+          />
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xs" onClick={handleSearch}>Submit</button>
+        </div>
+      </div>
 
-          <div className="flex justify-start bg-amber-50 gap-8">
-            <input
-              type="text"
-              placeholder="Enter Pokémon name..."
-              value={query}
-              onChange={(e) => setQuery(e.target.value)}
-              className="px-4 py-2 flex-1"
+      <div className="flex justify-center space-x-20">
+        <div className="text-center">
+          <div className="relative w-full max-w-md mx-auto">
+            {/* Base pokedex image */}
+            <img
+              src="/pokedex.png"
+              alt="pokedex-img"
+              className="w-full h-auto block"
             />
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xs" onClick={handleSearch}>Submit</button>
+            {/* Screen area (fixed zone) */}
+            <div className="absolute top-[30%] left-[5%] w-[45%] h-[32%] flex items-center justify-center overflow-hidden">
+              {pokemon && (
+                <img
+                  src={pokemon.sprite ?? ""}
+                  alt={pokemon.name}
+                  className="max-w-full max-h-full object-contain"
+                />
+              )}
+            </div>
           </div>
         </div>
 
-        <div className="relative w-full max-w-md mx-auto">
-          {/* Base pokedex image */}
-          <img
-            src="/pokedex.png"
-            alt="pokedex-img"
-            className="w-full h-auto block"
-          />
+        <div className="bg-white rounded-lg p-3 border-2 border-orange-300 w-full max-w-xl">
+          <div className="text-l font-semibold">
+            <h4 className="mb-4">Base Stats</h4>
 
-          {/* Screen area (fixed zone) */}
-          <div className="absolute top-[13%] left-[5%] w-[45%] h-[32%] flex items-center justify-center overflow-hidden">
-            {pokemon && (
-              <img
-                src={pokemon.sprite ?? ""}
-                alt={pokemon.name}
-                className="max-w-full max-h-full object-contain"
-              />
-            )}
+            {pokemon &&
+              Object.entries(pokemon.stats).map(([key, value]) => (
+                <div key={key} className="flex items-center gap-3">
+                  <div className="w-full mb-3">
+                    <div className="flex justify-between">
+                      <span>{formatStatName(key)}</span>
+                      <span className="w-10 text-right">{value}</span>
+                    </div>
+
+                    <div className="h-3 bg-gray-200 rounded">
+                      <div
+                        className="h-3 bg-black rounded"
+                        style={{ width: `${(value / 255) * 100}%` }}
+                      />
+                    </div>
+                  </div>
+                </div>
+              ))
+            }
           </div>
-
-          {pokemon && (
-            <div className="font-medium">
-              <div>HP: {pokemon.stats["hp"]}</div>
-              <div>Attack: {pokemon.stats["attack"]}</div>
-              <div>Defense: {pokemon.stats["defense"]}</div>
-              <div>SpAtk: {pokemon.stats["special-attack"]}</div>
-              <div>SpDef: {pokemon.stats["special-defense"]}</div>
-              <div>Speed: {pokemon.stats["speed"]}</div>
-              <div>Total Base Stats: {calculateTotalStats(pokemon)}</div>
-            </div>
-          )}
         </div>
       </div>
     </div>
--- frontend/src/utils/stats.ts
+++ frontend/src/utils/stats.ts
@@ -1,5 +1,18 @@
 import { type Pokemon } from "../api/pokemon";
 
+export function formatStatName(name: string) {
+  const map: Record<string, string> = {
+    hp: "Hp",
+    attack: "Atk",
+    defense: "Def",
+    "special-attack": "Sp.Atk",
+    "special-defense": "Sp.Def",
+    speed: "Spd"
+  };
+
+  return map[name] ?? name;
+}
+
 export function calculateTotalStats(pokemon: Pokemon | null): number {
   if (!pokemon) return 0;
   
